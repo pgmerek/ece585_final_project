8eabedfdcf0e202d3cf2c04210c2593111ba0f89
Patrick Gmerek <pgmerek@pdx.edu>
2018-11-04 13:42:56 -0800

    Started to populate some of the class functions.

--- simulator_code/header.h
+++ simulator_code/header.h
@@ -14,15 +14,39 @@
 #define DATA_NUM_SETS 16384
 #define DATA_BYTE_LINES 64
 
+// Declare an enum for the MESI bits
+enum enum_mesi
+{
+    MODIFIED    = 0;
+    INVALID     = 1;
+    SHARED      = 2;
+    EXCLUSIVE   = 3;
+};
+
+// Declare the necessary classes
 class cache
 {
     public:
         cache();
         ~cache();
+        int get_reads() const { return reads} 
+        int get_writes() const { return writes };
+        int hits();
+        int misses();
+        float hit_miss_ratio();
 
     private:
-
-
+        // Number of...
+        int hits;
+        int misses;
+        int reads;
+        int writes;
+        int operations;
+        // Cache parameters
+        int set_size;
+        int associativity;
+        // Pointer to the sets in the cache
+        set ** Sets;
 };
 
 class set
@@ -40,8 +64,18 @@ class line
     pubic:
         line();
         ~line();
+        int set_tag(int new_tag);
+        int set_lru(int new_lru);
+        int get_mesi(void) const;
+        int get_tag(void) const { return tag; }
+        int get_lru(void) const { return lru; }
 
     private:
-
+        int tag;
+        int lru;
+        enum_mesi mesi;
+        // Pointers to the next and previous lines relative to this line
+        line * next;
+        line * prev;
 };
 

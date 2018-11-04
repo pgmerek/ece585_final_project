2f695aa41ec8df289dfc44d692585ba7adf9edaa
Patrick Gmerek <pgmerek@pdx.edu>
2018-11-04 13:20:05 -0800

    Added all the source files I think we'll need. Also added a gitignore so that swap files aren't pushed.

--- simulator_code/line.cpp
+++ simulator_code/line.cpp
@@ -1,12 +1,7 @@
 /* ECE 485/585 Fall Term 2018 Final Project
- * Split L1 Cache simulation
+ * Split L1 Cache simulation, line class functions
  * Members: Patrick Gmerek, Stephen Poanessa, Emma Smith, Amanda Voegtlin
  * 4 November 2018
  */
 
 #include <header.h>
-
-int main(int arg c, char * argv[])
-{
-    
-

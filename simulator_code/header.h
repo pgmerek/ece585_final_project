b9003080dbe7f69b94ddca81f88462fdeb9ff72c
Patrick Gmerek <pgmerek@rita.cecs.pdx.edu>
2018-11-04 13:07:05 -0800

    Moved code out of project parent directory.

---
+++ simulator_code/header.h
@@ -0,0 +1,47 @@
+/* ECE 485/585 Fall Term 2018 Final Project
+ * Header file for main.cpp
+ * Members: Patrick Gmerek, Stephen Poanessa, Emma Smith, Amanda Voegtlin
+ * 4 November 2018
+ */
+
+#define CPU_BITS 32
+// Instruction cache constants
+#define INSTR_NUM_LINES 4
+#define INSTR_NUM_SETS 16384
+#define INSTR_BYTE_LINES 64
+// Data cache constants
+#define DATA_NUM_LINES 8
+#define DATA_NUM_SETS 16384
+#define DATA_BYTE_LINES 64
+
+class cache
+{
+    public:
+        cache();
+        ~cache();
+
+    private:
+
+
+};
+
+class set
+{
+    pubic:
+        set();
+        ~set();
+
+    private:
+
+};
+
+class line
+{
+    pubic:
+        line();
+        ~line();
+
+    private:
+
+};
+
